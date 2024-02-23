778f09f9cd81c4494e1d3c2fc5b268efed199a22
Trixie-Bea <kiokobeatrice710@gmail.com>
2024-02-23 13:49:57 +0300

    created a python file

---
+++ python_tasks/user_input.py
@@ -0,0 +1,5 @@
+name =input("what is your name?")
+age =int(input("How old are you?"))
+location = input("what is your location?")
+
+print(f"Hello{name.title()}, you are{age} years old and live in {location.title()}.")
