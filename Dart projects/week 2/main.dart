b3434c4c42aa85f0f3734dbb128454a9d39e8a38
Trixie-Bea <kiokobeatrice710@gmail.com>
2024-04-14 00:40:05 +0300

    created a dart file

---
+++ Dart projects/week 2/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Integer data type
+  int age = 24;
+  print('Age: $age'); //Output: Age: 24
+
+//string data type
+  String name = 'John Doe';
+  print('Name: $name'); //Output: John Doe
+
+  //Double data type
+  double otherNum = 19.95;
+  print("Double: $otherNum"); //output: Double: 19.95
+
+  //list Data type
+  List<String> Cities = ["Nairobi", "Mombasa", "Kisumu"];
+  print('Cities: $Cities'); // Output: Cities:[Nairobi, Mombasa, Kisumu]
+
+  //Map data type
+  Map<String, String> person = {
+    'name': 'Mary',
+    'age': '30',
+  };
+  print('person: $person'); //Output: person: {name:Mary,}
+}
